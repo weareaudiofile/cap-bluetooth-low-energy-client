0864e3b385b83380d8a1f1455fd493d34428270d
Eric Watson <wasnotrice@gmail.com>
2020-01-14 11:19:26 -0600

    WIP: Add iOS support

--- ios/Plugin/Plugin.m
+++ ios/Plugin/Plugin.m
@@ -4,5 +4,21 @@
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(BluetoothLEClient, "BluetoothLEClient",
-           CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(isAvailable, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(isEnabled, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(enable, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(scan, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(connect, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(discover, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(disconnect, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(read, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(write, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(readDescriptor, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(writeDescriptor, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(getServices, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(getService, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(getCharacteristics, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(getCharacteristic, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(enableNotifications, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(disableNotifications, CAPPluginReturnPromise);
 )
--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -1,5 +1,23 @@
 import Foundation
 import Capacitor
+import CoreBluetooth
+
+enum Key: String {
+    case isAvailable = "isAvailable"
+    case isEnabled = "isEnabled"
+    case enabled = "enabled"
+    case services = "services"
+}
+
+extension CAPPluginCall {
+    func setValue(_ value: Any, forKey key: Key) {
+        setValue(value, forKey: key.rawValue)
+    }
+
+    func getArray<T>(_ key: Key, _ type: T.Type) -> [T]? {
+        return getArray(key.rawValue, type)
+    }
+}
 
 /**
  * Please read the Capacitor iOS Plugin Development Guide
@@ -7,11 +25,92 @@ import Capacitor
  */
 @objc(BluetoothLEClient)
 public class BluetoothLEClient: CAPPlugin {
-    
-    @objc func echo(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
-        call.success([
-            "value": value
-        ])
+    let manager = CBCentralManager()
+    var state: CBManagerState = .unknown
+
+    @objc func isAvailable(_ call: CAPPluginCall) {
+        let isAvailable = manager.state != .unknown && manager.state != .unsupported
+        call.setValue(isAvailable, forKey: .isAvailable)
+        call.resolve()
     }
+
+    @objc func isEnabled(_ call: CAPPluginCall) {
+        let isEnabled = manager.state != .unknown && manager.state != .unauthorized
+        call.setValue(isEnabled, forKey: .isEnabled)
+        call.resolve()
+    }
+
+    @objc func enable(_ call: CAPPluginCall) {
+        if manager.state == .poweredOn {
+            call.setValue(true, forKey: .enabled)
+            call.resolve()
+        }
+
+        // TODO: Ask user to enable Bluetooth
+    }
+
+    @objc func scan(_ call: CAPPluginCall) {
+        let services: [CBUUID]? = call.getArray(.services, String.self)?.compactMap { CBUUID(string: $0) }
+        manager.scanForPeripherals(withServices: services, options: nil)
+    }
+
+    @objc func connect(_ call: CAPPluginCall) {}
+
+    @objc func discover(_ call: CAPPluginCall) {}
+
+    @objc func disconnect(_ call: CAPPluginCall) {}
+
+    @objc func read(_ call: CAPPluginCall) {}
+
+    @objc func write(_ call: CAPPluginCall) {}
+
+    @objc func readDescriptor(_ call: CAPPluginCall) {}
+
+    @objc func writeDescriptor(_ call: CAPPluginCall) {}
+
+    @objc func getServices(_ call: CAPPluginCall) {}
+
+    @objc func getService(_ call: CAPPluginCall) {}
+
+    @objc func getCharacteristics(_ call: CAPPluginCall) {}
+
+    @objc func getCharacteristic(_ call: CAPPluginCall) {}
+
+    @objc func enableNotifications(_ call: CAPPluginCall) {}
+
+    @objc func disableNotifications(_ call: CAPPluginCall) {}
+
+    private func set(call: CAPPluginCall, key: Key, value: Any) {
+        call.setValue(value, forKey: key.rawValue)
+    }
+}
+
+extension BluetoothLEClient: CBCentralManagerDelegate {
+    public func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        switch central.state {
+        case .poweredOn:
+            print("Bluetooth is on")
+            break
+        case .poweredOff:
+            print("Bluetooth is off")
+            break
+        case .resetting:
+            print("Bluetooth is resetting")
+            break
+        case .unauthorized:
+            print("Bluetooth is unauthorized")
+            break
+        case .unsupported:
+            print("Bluetooth is unsupported")
+            break
+        case .unknown:
+            print("Bluetooth state is unknown")
+            break
+        default:
+            print("Bluetooth entered unknown state")
+            break
+        }
+    }
+
+
 }
